6ac215bb5307328d1bcb444a5ef2571823cde126
Oskar Mikus <oskarmikus@proton.me>
2024-07-25 15:10:43 +0200

    Update conversion of the prostate 158 dataset
    Add resampling and preprocessing to the dataset as follows in the original paper & add validation to the fold 0

--- convert_prostate_158.py
+++ convert_prostate_158.py
@@ -1,13 +1,27 @@
 import os
 import shutil
+import SimpleITK as sitk
+import pandas as pd
+from tqdm import tqdm
+from center_crop import crop
+from nnunetv2.dataset_conversion.generate_dataset_json import generate_dataset_json
+
 
 T2_SEQ = 0
 ADC_SEQ = 1
 DWI_SEQ = 2
 
-source_train_dir = os.path.join('Prostate158', 'prostate158_train', 'train')
-source_test_dir = os.path.join('Prostate158', 'prostate158_test', 'prostate158_test', 'test')
-dest_dir = 'nnUNet_raw/Dataset158_Prostate158'
+source_train_dir = os.path.join('..', 'Prostate158', 'prostate158_train', 'train')
+source_test_dir = os.path.join('..', 'Prostate158', 'prostate158_test', 'prostate158_test', 'test')
+dest_dir = os.path.join('..', 'nnUNet_raw', 'Dataset158_Prostate158')
+
+train_csv = os.path.join(source_train_dir, '..', 'train.csv')
+train_valid_csv = os.path.join(source_train_dir, '..', 'valid.csv')
+test_csv = os.path.join(source_test_dir, '..', 'test.csv')
+
+train_df = pd.read_csv(train_csv)
+valid_df = pd.read_csv(train_valid_csv)
+test_df = pd.read_csv(test_csv)
 
 imagesTr_dir = os.path.join(dest_dir, 'imagesTr')
 labelsTr_dir = os.path.join(dest_dir, 'labelsTr')
@@ -19,11 +33,14 @@ os.makedirs(labelsTr_dir, exist_ok=True)
 os.makedirs(imagesTs_dir, exist_ok=True)
 os.makedirs(labelsTs_dir, exist_ok=True)
 
+
 def get_image_filename(case_number, sequence_number):
     return f'prostate_{case_number:03d}_{sequence_number:04d}.nii.gz'
 
+
 def get_label_filename(case_number):
     return f'prostate_{case_number:03d}.nii.gz'
+
 
 def process_case(case_path, case_number, images_dir, labels_dir):
     t2_path = os.path.join(case_path, 't2.nii.gz')
@@ -32,39 +49,71 @@ def process_case(case_path, case_number, images_dir, labels_dir):
     adc_tumor_reader1_path = os.path.join(case_path, 'adc_tumor_reader1.nii.gz')
     adc_tumor_reader_1_empty_path = os.path.join(case_path, 'empty.nii.gz')
 
+    t2_out_path = os.path.join(images_dir, get_image_filename(case_number, T2_SEQ))
+    adc_out_path = os.path.join(images_dir, get_image_filename(case_number, ADC_SEQ))
+    dwi_out_path = os.path.join(images_dir, get_image_filename(case_number, DWI_SEQ))
+    adc_tumor_reader1_out_path = os.path.join(labels_dir, get_label_filename(case_number))
+
     if os.path.exists(t2_path):
-        shutil.copy(t2_path, os.path.join(images_dir, get_image_filename(case_number, T2_SEQ)))
+        shutil.copy(t2_path, t2_out_path)
     else:
         raise FileNotFoundError(f'No T2w file found for case {case_number}!')
 
+    reference = sitk.ReadImage(t2_path)
+
+    def preprocess(img_path, out_path):
+        target = sitk.ReadImage(img_path)
+        resampled = sitk.Resample(target, reference)
+        cropped = crop(resampled, (.25, .25, .0))
+        sitk.WriteImage(cropped, out_path)
+
     if os.path.exists(adc_path):
-        shutil.copy(adc_path, os.path.join(images_dir, get_image_filename(case_number, ADC_SEQ)))
+        preprocess(adc_path, adc_out_path)
     else:
         raise FileNotFoundError(f'No ADC file found for case {case_number}!')
 
     if os.path.exists(dwi_path):
-        shutil.copy(dwi_path, os.path.join(images_dir, get_image_filename(case_number, DWI_SEQ)))
+        preprocess(dwi_path, dwi_out_path)
     else:
         raise FileNotFoundError(f'No DWI file found for case {case_number}!')
 
     if os.path.exists(adc_tumor_reader1_path):
-        shutil.copy(adc_tumor_reader1_path, os.path.join(labels_dir, get_label_filename(case_number)))
+        preprocess(adc_tumor_reader1_path, adc_tumor_reader1_out_path)
     elif os.path.exists(adc_tumor_reader_1_empty_path):
-        shutil.copy(adc_tumor_reader_1_empty_path, os.path.join(labels_dir, get_label_filename(case_number)))
+        preprocess(adc_tumor_reader_1_empty_path, adc_tumor_reader1_out_path)
     else:
         raise FileNotFoundError(f'No ADC Tumor Reader file found for case {case_number}!')
 
-def process_directory(source_dir, images_dir, labels_dir, start_case_number=1):
-    files = [f for f in os.listdir(source_dir) if not f.startswith('.')]
-    for case_number, case_dir in enumerate(sorted(files), start=start_case_number):
-        case_path = os.path.join(source_dir, case_dir)
-        if os.path.isdir(case_path):
-            process_case(case_path, case_number, images_dir, labels_dir)
-        else:
-            raise NotADirectoryError(f'{case_path} is not a directory!')
 
-process_directory(source_train_dir, imagesTr_dir, labelsTr_dir, start_case_number=20)
-print("Training files copied and renamed successfully.")
+def process_df(df, src_dir, images_dir, labels_dir, start_case=1):
+    with tqdm(total=len(df)) as bar:
+        for row_idx, row in df[['ID', 't2', 'adc', 'dwi', 'adc_tumor_reader1']].iterrows():
+            case_path = os.path.join(src_dir, str(row['ID']).zfill(3))
+            if os.path.isdir(case_path):
+                process_case(case_path, row_idx + start_case, images_dir, labels_dir)
+                bar.update(1)
+            else:
+                raise NotADirectoryError(f'{case_path} is not a directory!')
 
-process_directory(source_test_dir, imagesTs_dir, labelsTs_dir)
-print("Test files copied and renamed successfully.")
+
+if __name__ == "__main__":
+    process_df(test_df, source_test_dir, imagesTs_dir, labelsTs_dir)
+    process_df(valid_df, source_train_dir, imagesTr_dir, labelsTr_dir)
+    process_df(train_df, source_train_dir, imagesTr_dir, labelsTr_dir, start_case=21)
+    generate_dataset_json(
+        dest_dir,
+        channel_names={
+            T2_SEQ: 'T2',
+            ADC_SEQ: 'ADC',
+            DWI_SEQ: 'DWI'
+        },
+        file_ending=".nii",
+        labels={
+            "background": 0,
+            "tumor": 1,
+        },
+        dataset_name='Dataset158_Prostate158',
+        num_training_cases=139,
+
+    )
+
